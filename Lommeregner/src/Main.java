359298281bb8bcac0bfd523b2b059a9a2ce12a78
Zlow <asger@bissen.dk>
2023-01-17 09:24:33 +0100

    final prob

--- Lommeregner/src/Main.java
+++ Lommeregner/src/Main.java
@@ -2,19 +2,23 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Main implements KeyListener {
-    public static JButton B1, B2, B3, B4, B5, B6, B7, B8, B9, B0, BPlus, BMinus, BDivide, BMultiply, BEquals, BClear, BComma, BDelete;
+class Main {
+    public static JButton B1, B2, B3, B4, B5, B6, B7, B8, B9, B0, BPlus, BMinus, BDivide, BMultiply, BEquals, BClear, BComma, BDelete, BTrigonometri, BAndet, BCos, BTan, BSin, BX2, BPi, BSqrt;
     public static JTextField Result;
     public static Color symbolFarve = new Color(50,50,50);
     public static Color talFarve = new Color(59,59,59);
     static Font fontResult = new Font("SansSerif", Font.BOLD, 40);
     static Font fontText = new Font("SansSerif", Font.BOLD, 20);
+    static Font dropdownText = new Font("SansSerif", Font.BOLD, 10);
+    public static boolean trigonometri = false;
+    public static boolean andet = false;
 
     static void MainPanel(){
         JFrame frame = new JFrame("Lommeregner");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setBackground(new Color(32,32,32));
         frame.setSize(340, 443);
+        frame.setResizable(false);
 
         Result = new JTextField(20);
         Result.setBounds(2,2,315,146);
@@ -22,9 +26,71 @@ public class Main implements KeyListener {
         Result.setForeground(Color.WHITE);
         Result.setHorizontalAlignment(SwingConstants.RIGHT);
         Result.setFont(fontResult);
-
-
         Result.setBorder(BorderFactory.createEmptyBorder());
+
+        //Række 0
+        BTrigonometri = new JButton("Trigonometri ↓");
+        BTrigonometri.setBounds(2,150,162,24);
+        BTrigonometri.setBackground(symbolFarve);
+        BTrigonometri.setForeground(Color.WHITE);
+        BTrigonometri.setBorder(BorderFactory.createEmptyBorder());
+        BTrigonometri.setFont(dropdownText);
+
+        //Trigonometri - Toggle Frame
+        BCos = new JButton("Cos");
+        BCos.setBounds(2,202,80,50);
+        BCos.setBackground(talFarve);
+        BCos.setForeground(Color.WHITE);
+        BCos.setBorder(BorderFactory.createEmptyBorder());
+        BCos.setFont(fontText);
+        BCos.setVisible(false);
+        BSin = new JButton("Sin");
+        BSin.setBounds(84,202,80,50);
+        BSin.setBackground(talFarve);
+        BSin.setForeground(Color.WHITE);
+        BSin.setBorder(BorderFactory.createEmptyBorder());
+        BSin.setFont(fontText);
+        BSin.setVisible(false);
+        BTan = new JButton("Tan");
+        BTan.setBounds(166,202,80,50);
+        BTan.setBackground(talFarve);
+        BTan.setForeground(Color.WHITE);
+        BTan.setBorder(BorderFactory.createEmptyBorder());
+        BTan.setFont(fontText);
+        BTan.setVisible(false);
+
+
+        BAndet = new JButton("Andet ↓");
+        BAndet.setBounds(2,176,162,24);
+        BAndet.setBackground(symbolFarve);
+        BAndet.setForeground(Color.WHITE);
+        BAndet.setBorder(BorderFactory.createEmptyBorder());
+        BAndet.setFont(dropdownText);
+
+        //Andet - Toggle Frame
+        BX2 = new JButton("x²");
+        BX2.setBounds(2,202,80,50);
+        BX2.setBackground(talFarve);
+        BX2.setForeground(Color.WHITE);
+        BX2.setBorder(BorderFactory.createEmptyBorder());
+        BX2.setFont(fontText);
+        BX2.setVisible(false);
+
+        BPi = new JButton("\uD835\uDF0B");
+        BPi.setBounds(84,202,80,50);
+        BPi.setBackground(talFarve);
+        BPi.setForeground(Color.WHITE);
+        BPi.setBorder(BorderFactory.createEmptyBorder());
+        BPi.setFont(fontText);
+        BPi.setVisible(false);
+
+        BSqrt = new JButton("\u221A");
+        BSqrt.setBounds(166,202,80,50);
+        BSqrt.setBackground(talFarve);
+        BSqrt.setForeground(Color.WHITE);
+        BSqrt.setBorder(BorderFactory.createEmptyBorder());
+        BSqrt.setFont(fontText);
+        BSqrt.setVisible(false);
 
         //Række 1
         BClear = new JButton("C");
@@ -279,6 +345,133 @@ public class Main implements KeyListener {
         });
 
 
+
+        BTrigonometri.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(trigonometri){
+                    Hide();
+
+                } else if (andet) {
+                    Hide();
+
+                    BTan.setVisible(true);
+                    BCos.setVisible(true);
+                    BSin.setVisible(true);
+
+                    B1.setVisible(false);
+                    B2.setVisible(false);
+                    B3.setVisible(false);
+
+                    trigonometri = true;
+                } else if(!trigonometri){
+                    BTan.setVisible(true);
+                    BCos.setVisible(true);
+                    BSin.setVisible(true);
+
+                    B1.setVisible(false);
+                    B2.setVisible(false);
+                    B3.setVisible(false);
+
+                    trigonometri = true;
+                }
+
+
+            }
+        });
+
+        BTan.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double f1 = Double.parseDouble(Result.getText());
+                double a = Math.tan(f1*Math.PI/180);
+                double result = Math.round(a * 1000.0) / 1000.0;
+                Result.setText(String.valueOf(result));
+
+            }
+        });
+
+        BCos.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double f1 = Double.parseDouble(Result.getText());
+                double a = Math.cos(f1*Math.PI/180);
+                double result = Math.round(a * 1000.0) / 1000.0;
+                Result.setText(String.valueOf(result));
+
+            }
+        });
+
+        BSin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double f1 = Double.parseDouble(Result.getText());
+                double a = Math.sin(f1*Math.PI/180);
+                double result = Math.round(a * 1000.0) / 1000.0;
+                Result.setText(String.valueOf(result));
+
+            }
+        });
+
+        BAndet.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(andet){
+                    Hide();
+                } else if (trigonometri){
+                    Hide();
+
+                    BX2.setVisible(true);
+                    BPi.setVisible(true);
+                    BSqrt.setVisible(true);
+
+                    B1.setVisible(false);
+                    B2.setVisible(false);
+                    B3.setVisible(false);
+
+                    andet = true;
+                } else if( !andet){
+                    BX2.setVisible(true);
+                    BPi.setVisible(true);
+                    BSqrt.setVisible(true);
+
+                    B1.setVisible(false);
+                    B2.setVisible(false);
+                    B3.setVisible(false);
+
+                    andet = true;
+                }
+            }
+        });
+
+        BX2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double x = Double.parseDouble(Result.getText());
+                double result = x*x;
+                Result.setText(String.valueOf(result));
+
+            }
+        });
+
+        BPi.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double result = Math.round((Math.PI) * 1000.0) / 1000.0;
+                Result.setText(Result.getText() + result);
+            }
+        });
+
+        BSqrt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double x = Double.parseDouble(Result.getText());
+                double result = Math.round((Math.sqrt(x)) * 1000.0) / 1000.0;
+                Result.setText(String.valueOf(result));
+            }
+        });
+
+
         frame.add(B1);
         frame.add(B2);
         frame.add(B3);
@@ -298,6 +491,14 @@ public class Main implements KeyListener {
         frame.add(BComma);
         frame.add(Result);
         frame.add(BDelete);
+        frame.add(BTrigonometri);
+        frame.add(BAndet);
+        frame.add(BCos);
+        frame.add(BTan);
+        frame.add(BSin);
+        frame.add(BX2);
+        frame.add(BPi);
+        frame.add(BSqrt);
 
         frame.setLayout(null);
         frame.setVisible(true);
@@ -310,6 +511,7 @@ public class Main implements KeyListener {
         String[] s2 = s.split("\\-");
         String[] s3 = s.split("\\*");
         String[] s4 = s.split("\\/");
+
         if(s1.length == 2){
             double d1 = Double.parseDouble(s1[0]);
             double d2 = Double.parseDouble(s1[1]);
@@ -334,6 +536,22 @@ public class Main implements KeyListener {
             double d3 = d1 / d2;
             Result.setText(Double.toString(d3));
         }
+    }
+
+    public static void Hide(){
+        BTan.setVisible(false);
+        BCos.setVisible(false);
+        BSin.setVisible(false);
+        BX2.setVisible(false);
+        BPi.setVisible(false);
+        BSqrt.setVisible(false);
+
+        B1.setVisible(true);
+        B2.setVisible(true);
+        B3.setVisible(true);
+
+        trigonometri = false;
+        andet = false;
     }
 
 
